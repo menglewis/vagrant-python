cfa8bcddc888684db6b475b9b7bb40e2273edfad
David Lewis <meng.lewis@gmail.com>
2014-03-01 08:38:15 -0600

    Fixed an error with pathing on the ssh batch script

--- ssh_vagrant.bat
+++ ssh_vagrant.bat
@@ -1,1 +1,1 @@
-"%ProgramFiles(x86)%\Git\bin\ssh.exe" -p 2222 -X -i %USERPROFILE%\.vagrant.d\insecure_private_key -t vagrant@localhost
+"%ProgramFiles(x86)%\Git\bin\ssh.exe" -p 2222 -X -i "%USERPROFILE%\.vagrant.d\insecure_private_key" -t vagrant@localhost
