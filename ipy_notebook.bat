9960d8f83aea8822bf84029c886fcd6d3b1f0d82
David Lewis <meng.lewis@gmail.com>
2014-03-01 08:38:01 -0600

    Added batch file for starting iPython notebook remotely

---
+++ ipy_notebook.bat
@@ -0,0 +1,6 @@
+set ROOT_DIR=%~dp0..
+set PYTHONPATH=%ROOT_DIR%\python;%PYTHONPATH%
+
+set EDITOR=gvim.exe
+
+ipython notebook --port=8888 --ip=0.0.0.0 --no-browser --pylab=inline --notebook-dir=%ROOT_DIR%\notebooks --profile sympy --ConsoleWidget.editor=%EDITOR%
